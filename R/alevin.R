9eb23fa3ec610bebf53c079cd86b97c102d1bccb
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-05-16 19:39:11 -0400

    adding some missing things from wiki

--- R/alevin.R
+++ R/alevin.R
@@ -3,17 +3,19 @@
 NULL
 
 #' @inheritParams Seurat::CreateSeuratObject
-#' Load Alevin Counts data from a EDS file
+#' Load alevin Counts data from an EDS file
 #'
 #' This is a wrapper around ReadCounts from AlevinRtools
 #'
 #' @param file Path to `quants_mat.gz` file
 #'
-#' @importFrom utils capture.output
-#'
+#' @return Returns a Seurat object with alevin counts
+#' @seealso \code{\link[alevin]{alevin}}
+#' @author Avi Srivastava
+#' @references Srivastava, Avi, et al. "Alevin efficiently 
+#' estimates accurate gene abundances from dscRNA-seq data." 
+#' Genome biology 20.1 (2019): 65.
 #' @export
-#'
-#'
 ReadAlevin <- function(file) {
     CheckPackage(package = 'k3yavi/alevin-Rtools', repository = 'github')
     counts <- AlevinRtools::ReadCounts(file)
