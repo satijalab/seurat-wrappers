8a6b8aec5578aaa511303466935d52d5a4b81e03
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-01-15 14:30:56 -0600

    Revised the FitTopicModel roxygen2 docs.

--- R/fasttopics.R
+++ R/fasttopics.R
@@ -1,26 +1,29 @@
+# TO DO:
+#  - Implement FitPoissonNMF.
+#  - Implement vignette.
+
 #' @title Fit a Multinomial Topic Model Using fastTopics
 #'
-#' @description Fits a multinomial topic model to the count data,
+#' @description Fits a multinomial topic model to the raw count data,
 #'   hiding most of the complexities of model fitting. The default
 #'   optimization settings used here are intended to work well in a wide
 #'   range of data sets, although some fine-tuning may be needed for
-#'   more difficult cases.
+#'   larger or more complex data sets.
 #'
 #' @details The topic model can be understood as a dimensionality
 #' reduction, in which the mixture proportions matrix, \code{L}, an n
 #' x k matrix, defines a projection of the cells onto a
 #' (k–1)-dimension space (n is the number of cells and k is the number
-#' of topics). Therefore, we use the mixture proportions to define the
-#' \dQuote{cell embedding} in the \code{DimReduc} object.
+#' of topics). The topic mixture proportions define the \dQuote{cell
+#' embedding} in the \code{DimReduc} object.
 #'
-#' There is a confusing bit of terminology here, because "loadings"
-#' means something different depending on the context In fastTopics,
+#' \emph{A warning about confusing terminology:} In fastTopics,
 #' the \code{L} matrix is sometimes referred to as the "loadings
-#' matrix" (this is the convention used in factor analysis), but it is
-#' \emph{not} the same as the "feature loadings" matrix used here,
-#' which adopts the convention used for principal component
-#' analysis. To avoid confusion, we only refer to \code{L} as the
-#' mixture proportions matrix.
+#' matrix" (because this is the convention used in factor
+#' analysis). However, this is \emph{not} the same as the "feature
+#' loadings" matrix used in Seurat, which adopts the convention used n
+#' principal component analysis. To avoid confusion, we refer to
+#' \code{L} as the mixture proportions matrix.
 #' 
 #' See \code{\link[fastTopics]{fit_topic_model}} for more details.
 #' 
@@ -32,8 +35,9 @@
 #'   assay of the object.
 #'
 #' @param features A list of features to use for fitting the model. If
-#'   \code{features = NULL}, \emph{all} features are used; see
-#'   \code{\link[Seurat]{VariableFeatures}}. NOTE: EXPAND ON THIS.
+#' \code{features = NULL}, \emph{all} features are used; in
+#' particular, ; see \code{\link[Seurat]{VariableFeatures}} is \emph{not
+#' used to pre-select features.
 #'
 #' @param reduction.name Name of the outputted reduction.
 #'
@@ -47,36 +51,34 @@
 #' @param \dots Additional arguments passed to \code{fit_topic_model};
 #'   see \code{\link[fastTopics]{fit_topic_model}} for details.
 #'
-#' @return A Seurat object with the multinomial topic model fit stored
-#' as a \code{\link[Seurat]{DimReduc}} object. The cell embeddings
-#' are the mixture proportions, stored as an n x k matrix in the
-#' \code{cell.embeddings} slot, where n is the number of cells, and k
-#' is the number of topics.  This is the same as the \code{L} matrix
-#' output in \code{fit_topic_model}.
-#'
-#' The feature loadings (the \code{feature.loadings} slot) are an m x
-#' k matrix in which column i is the vector of relative gene
-#' expression levels in topic i. This is the same as the \code{F}
-#' output in \code{fit_topic_model}.
+#' @return A Seurat object, in which the multinomial topic model fit
+#' is stored as a Seurat \code{\link[Seurat]{DimReduc}} object. The
+#' cell embeddings (stored in the \code{cell.embeddings} slot) are the
+#' mixture proportions; this is the n x k matrix \code{L} outputted by
+#' \code{fit_topic_model} (n is the number of cells and k is the
+#' number of topics).
+#' 
+#' The feature loadings (stored in the \code{feature.loadings} slot)
+#' are an m x k matrix of relative expression levels; this is the
+#' matrix \code{F} outputted by \code{fit_topic_model}.
 #'
 #' Apply \code{\link[Seurat]{Misc}} to the \code{DimReduce} object to
 #' access the \dQuote{"multinom_topic_model_fit"} object outputted by
 #' \code{\link[fastTopics]{fit_topic_model}}, which contains more
-#' information about the model fit; see the example for an
-#' illustration of how to do this, and see
+#' information about the multinomial topic model fit; see the example
+#' here for an illustration of how to access the model fit, and see
 #' \code{\link[fastTopics]{fit_topic_model}} more information about
 #' the "multinom_topic_model_fit" object.
 #'
-#' An additional PCA dimension reduction is provided to, for example,
-#' quickly visualize the cells using
-#' \code{\link[Seurat]{DimPlot}}. The principal components are
-#' computed from the topic model mixture proportions. However, this is
-#' for convenience only, and we recommend extracting the
-#' \dQuote{"multinom_topic_model_fit"} object and dedicated
-#' visualization tools such \code{\link[fastTopics]{structure_plot}}
-#' that are provided in the fastTopics package.
-#' 
-#' NOTE: There are only k - 1 PCs.
+#' An additional PCA dimension reduction on \code{k-1} dimensions is
+#' provided. This could be useful, for example, to quickly visualize
+#' the cells using \code{\link[Seurat]{DimPlot}}. The principal
+#' components are computed from the topic mixture
+#' proportions. However, this reduction is provided for convenience
+#' only, and we recommend to extract the
+#' \dQuote{"multinom_topic_model_fit"} object and use the dedicated
+#' visualization tools that are provided in the fastTopics package
+#' such as \code{\link[fastTopics]{structure_plot}}.
 #' 
 #' @author Peter Carbonetto
 #'
@@ -96,20 +98,21 @@
 #' data(pbmc_small)
 #'
 #' # Fit the multinomial topic model to the raw UMI count data; no
-#' # pre-processing is needed. 
+#' # pre-processing or pre-selection of genes is needed.
 #' pbmc_small <- FitTopicModel(pbmc_small,k = 3)
 #'
 #' # This plot shows the cells projected onto the 2 principal
-#' # components (PCs) of the topic model mixture proportions.
+#' # components (PCs) of the topic mixture proportions.
 #' DimPlot(pbmc_small,reduction = "pca_topics")
 #'
-#' # Compare against PCA performed on the transformed count data.
+#' # Compare this against the top two PCs of the transformed count
+#' # data.
 #' DimPlot(pbmc_small,reduction = "pca")
 #'
 #' # Once fitted topic model is extracted, many functions from the
-#' # fastTopics package can be used. For example, the Structure plot
-#' # provides an evocative visual summary of the estimated mixture
-#' # proportions for each cell.
+#' # fastTopics package can be used for analysis and visualization. For
+#' # example, the Structure plot provides an evocative visual summary of
+#' # the estimated mixture proportions for each cell.
 #' fit <- Misc(Reductions(pbmc_small,"multinom_topic_model"))
 #' structure_plot(fit,grouping = Idents(pbmc_small),gap = 5)
 #'
