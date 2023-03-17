20559ce6f893e12cfc8324df2467e0a8c407b120
Peter Carbonetto <peter.carbonetto@gmail.com>
2023-03-17 14:12:33 -0500

    Updated roxygen2 for FitPoissonNMF and FitTopicModel.

--- R/fasttopics.R
+++ R/fasttopics.R
@@ -1,9 +1,6 @@
 #' @title Fit a Non-negative Matrix Factorization using fastTopics
 #'
-#' @description Approximate the raw count data \code{X} by the
-#'   non-negative matrix factorization \code{tcrossprod(L,F)}, in which
-#'   the quality of the approximation is measured by a
-#'   \dQuote{divergence} criterion; equivalently, optimize the
+#' @description Optimize the
 #'   likelihood under a Poisson model of the count data, \code{X}, in
 #'   which the Poisson rates are given by \code{tcrossprod(L,F)}.
 #'
@@ -69,7 +66,7 @@
 #' Apply \code{\link[Seurat]{Misc}} to the \code{DimReduce} object to
 #' access the \dQuote{"poisson_nmf_fit"} object outputted by
 #' \code{\link[fastTopics]{fit_poisson_nmf}}; see the example for an
-#' illustration.
+#' illustration of how to do this.
 #'
 #' An additional PCA dimension reduction computed from the k loadings
 #' is provided.
@@ -77,9 +74,13 @@
 #' @author Peter Carbonetto
 #'
 #' @references
-#'   Lee, D. D. and Seung, H. S. (2001). Algorithms for non-negative
-#'   matrix factorization. In \emph{Advances in Neural Information
-#'   Processing Systems} \bold{13}, 556–562.
+#' D. D. Lee and H. S. Seung (2001). Algorithms for non-negative
+#' matrix factorization. In \emph{Advances in Neural Information
+#' Processing Systems} \bold{13}, 556–562.
+#'
+#' P. Carbonetto, A. Sarkar, Z. Wang and M. Stephens (2021).
+#' Non-negative matrix factorization algorithms greatly improve
+#' topic model fits. \emph{arXiv} 2105.13440.
 #'
 #' @seealso \code{\link{FitTopicModel}},
 #'   \code{\link{PerformGoMDEAnalysis}},
@@ -96,7 +97,7 @@
 #' pbmc_small <- FitPoissonNMF(pbmc_small,k = 3,numiter = 20)
 #'
 #' # Improve the fit by running another 20 updates.
-#' pbmc_small <- FitPoissonNMF(pbmc_small,k = 3,numiter = 20)
+#' pbmc_small <- FitPoissonNMF(pbmc_small,numiter = 20)
 #'
 #' # This plot shows the cells projected onto the 2 principal
 #' # components (PCs) of the topic mixture proportions.
@@ -148,6 +149,9 @@ FitPoissonNMF <- function (object, k, assay = NULL, features = NULL,
     fit <- fit_poisson_nmf(X,fit0 = fit0,numiter = numiter,method = method,
                            init.method = init.method,control = control,
                            verbose = verbose,...)
+    if (!missing(k))
+      warning("Chosen value of k may conflict with existing fit")
+    k <- ncol(fit$F)
   } else
     fit <- fit_poisson_nmf(X,k,numiter = numiter,method = method,
                            init.method = init.method,control = control,
@@ -177,26 +181,24 @@ FitPoissonNMF <- function (object, k, assay = NULL, features = NULL,
 
 #' @title Fit a Multinomial Topic Model using fastTopics
 #'
-#' @description Fits a multinomial topic model to the raw count data,
-#'   hiding most of the complexities of model fitting. The default
-#'   optimization settings used here are intended to work well in a wide
-#'   range of data sets, although some fine-tuning may be needed for
-#'   larger or more complex data sets.
+#' @description Fits a multinomial topic model to the raw count data.
+#'   The default optimization settings used here are intended to work
+#'   well in a wide range of data sets, although some fine-tuning may be
+#'   needed for larger or more complex data sets.
 #'
-#' @details The topic model can be understood as a dimensionality
-#' reduction, in which the mixture proportions matrix, \code{L}, an n
-#' x k matrix, defines a projection of the cells onto a
-#' (k–1)-dimension space (n is the number of cells and k is the number
-#' of topics). The topic mixture proportions define the \dQuote{cell
-#' embedding} in the \code{DimReduc} object.
+#' @details The topic model can be viewed as a dimensionality reduction
+#' in which the topic proportions matrix, \code{L} (an n x k matrix,
+#' where n is the number of cells and k is the number of topics),
+#' defines a projection of the cells onto a k-dimensional embedding
+#' (strictly speaking, it is a (k-1)-dimensional embedding).  The topic
+#' proportions define the \dQuote{cell embedding} in the
+#' \code{DimReduc} object.
 #'
-#' \emph{A warning about confusing terminology:} In fastTopics,
-#' the \code{L} matrix is sometimes referred to as the "loadings
-#' matrix" (because this is the convention used in factor
-#' analysis). However, this is \emph{not} the same as the "feature
-#' loadings" matrix used in Seurat, which adopts the convention used n
-#' principal component analysis. To avoid confusion, we refer to
-#' \code{L} as the mixture proportions matrix.
+#' \emph{A warning about terminology:} In fastTopics, the \code{L}
+#' matrix is sometimes called the \dQuote{loadings matrix}. However,
+#' this is not the same as the \dQuote{feature loadings} matrix in
+#' Seurat. To avoid confusion, we instead prefer to refer to \code{L}
+#' as the topic proportions matrix.
 #' 
 #' See \code{\link[fastTopics]{fit_topic_model}} for more details.
 #' 
@@ -231,8 +233,8 @@ FitPoissonNMF <- function (object, k, assay = NULL, features = NULL,
 #' @return A Seurat object, in which the multinomial topic model fit
 #' is stored as a Seurat \code{\link[Seurat]{DimReduc}} object. The
 #' cell embeddings (stored in the \code{cell.embeddings} slot) are the
-#' mixture proportions; this is the n x k matrix \code{L} outputted by
-#' \code{fit_topic_model} (n is the number of cells and k is the
+#' topic proportions; this is the n x k matrix \code{L} outputted by
+#' \code{fit_topic_model} (n is the number of cells, k is the
 #' number of topics).
 #' 
 #' The feature loadings (stored in the \code{feature.loadings} slot)
@@ -241,29 +243,32 @@ FitPoissonNMF <- function (object, k, assay = NULL, features = NULL,
 #' matrix \code{F} outputted by \code{fit_topic_model}.
 #'
 #' Apply \code{\link[Seurat]{Misc}} to the \code{DimReduce} object to
-#' access the \dQuote{"multinom_topic_model_fit"} object outputted by
-#' \code{\link[fastTopics]{fit_topic_model}}, which contains more
-#' information about the multinomial topic model fit; see the example
-#' here for an illustration of how to access the model fit, and see
-#' \code{\link[fastTopics]{fit_topic_model}} more information about
-#' the "multinom_topic_model_fit" object.
+#' access the \dQuote{multinom_topic_model_fit} object outputted by
+#' \code{\link[fastTopics]{fit_topic_model}} which contains more
+#' information about the topic model fit; see the example for an
+#' illustration, and see \code{\link[fastTopics]{fit_topic_model}} more
+#' information about the \dQuote{multinom_topic_model_fit} object.
 #'
-#' An additional PCA dimension reduction on \code{k-1} dimensions,
-#' computed from the mixture proportions, is provided. This could be
-#' useful, for example, to quickly visualize the cells using
-#' \code{\link[Seurat]{DimPlot}}. The principal components are
-#' computed from the topic mixture proportions. However, this
-#' reduction is provided for convenience only, and we recommend to
-#' extract the \dQuote{"multinom_topic_model_fit"} object and use the
-#' dedicated visualization tools that are provided in the fastTopics
-#' package such as \code{\link[fastTopics]{structure_plot}}.
+#' An additional PCA dimension reduction computed from the topic
+#' proportions is provided. This could be useful, for example, to
+#' quickly visualize the cells using \code{\link[Seurat]{DimPlot}}. The
+#' principal components are computed from the topic mixture
+#' proportions. However, this reduction is provided for convenience
+#' only, and we recommend to extract the
+#' \dQuote{multinom_topic_model_fit} object and use the dedicated
+#' visualization tools that are provided in fastTopics such as
+#' \code{\link[fastTopics]{structure_plot}}.
 #' 
 #' @author Peter Carbonetto
 #'
 #' @references
-#' Dey, K. K., Hsiao, C. J. and Stephens, M. (2017). Visualizing the
+#' K. K. Dey, C. J. Hsiao and M. Stephens (2017). Visualizing the
 #' structure of RNA-seq expression data using grade of membership
 #' models. \emph{PLoS Genetics} \bold{13}, e1006599.
+#'
+#' P. Carbonetto, A. Sarkar, Z. Wang and M. Stephens (2021).
+#' Non-negative matrix factorization algorithms greatly improve
+#' topic model fits. \emph{arXiv} 2105.13440.
 #'
 #' @seealso \code{\link{FitPoissonNMF}},
 #'   \code{\link{PerformGoMDEAnalysis}},
@@ -322,9 +327,6 @@ FitTopicModel <- function (object, k = 3, assay = NULL, features = NULL,
   features <- colnames(X)
   
   # Fit the multinomial topic model using fastTopics.
-  if (verbose != "none")
-    cat(sprintf(paste("Fitting topic model with k = %d topics to %d x %d",
-                      "counts matrix.\n"),k,nrow(X),ncol(X)))
   fit <- fit_topic_model(X,k,verbose = verbose,...)
   class(fit) <- c("list","multinom_topic_model_fit")
 
@@ -361,7 +363,11 @@ FitTopicModel <- function (object, k = 3, assay = NULL, features = NULL,
 #'   \code{\link[fastTopics]{de_analysis}}.
 #'
 #' @return Describe the return value here.
-#' 
+#'
+#' @seealso
+#' \code{\link{FitPoissonNMF}},
+#' \code{\link{FitTopicModel}}
+#'
 #' @export
 #' 
 PerformGoMDEAnalysis <- function (object, ...) {
